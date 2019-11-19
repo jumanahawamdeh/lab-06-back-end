8c52f8ae1835bdc5655978bfb519ed7d078b81ed
Your Name In Single Quotes.jumana <your email here in single quotes.mjumana28@yahoo.com>
2019-11-19 17:32:26 +0200

    new edites

--- server.js
+++ server.js
@@ -1,65 +1,39 @@
-'use strict';
-
+'use strict'
 // DOTENV (Read our Environment Variables) -- UpperCase
-require('dotenv').config();
-
-// Express Server
-// Express does all the headers (envelope stuff)
+require('dotenv').config()
 const express = require('express');
-
 // CORS = Cross Origin Resource Sharing
 const cors = require('cors');
-
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 3000;
 const server = express();
-
 server.use( cors() );
-
-// Home Route (http://localhost:3000/)
-server.get('/', (request, response) => {
-  console.log(request);
-  response.status(200).send('You did a great job');
-});
-
-/*
-Object should look like this:
-{
-  "search_query": "seattle",
-  "formatted_query": "Seattle, WA, USA",
-  "latitude": "47.606210",
-  "longitude": "-122.332071"
-}
- */
-server.get('/location', (request, response) => {
-  const locationData = require('./data/geo.json');
-  const location = new Location(locationData);
-  response.status(200).json(location);
-});
-
-function Location( data ) {
-  this.search_query = 'lynnwood';
-  this.formatted_query = data.results[0].formatted_address;
-  this.latitude = data.results[0].geometry.location.lat;
-  this.longitude = data.results[0].geometry.location.lng;
-}
-
-// /weather route
-// Weather()
-
-// Force an Error to Happen (http://localhost:3000/boo)
-server.get('/boo', (request,response) => {
-  throw new Error('Whoops');
-});
-
-// When an error happens ...
-server.use('*', (request, response) =>{
-  response.status(404).send('Not Found');
-});
-
-server.use( (error, request, response) => {
-  response.status(500).send(error);
-});
-
-
-server.listen( PORT, () => console.log(`App listening on ${PORT}`));
+server.get('/location', locationHandler);
+function locationHandler (request, response) {
+   let locationData= getLocation(request.query.data);
+   response.status(200).json(locationData);
+ };
+ function getLocation(city){
+let data = require('./data/geo.json');
+  return new Location(city,data);
+ }
+ function Location (data) {
+     this.search_query = city;
+     this.formatted_query = geoData.results[0].formatted_address;
+     this.latitude = geoData.results[0].geometry.location.lat;
+     this.longitude = geoData.results[0].geometry.location.lng;
+   }
+ /////////////////////////////////////////////////////////
+ function weatherHandler (request, response) {
+   let weatehrData= getWeather(request.query.data);
+   response.status(200).json(weatherData);
+ };
+ function getWeather(city){
+   let data = require('./data/darksky.json');
+   return data.daily.data.map ((day) => {
+   return new Weather(day);
+   });
+    }
+    function Weather (day) {
+        this.forecast = day.summary;
+        this.time = new Date (day.time*1000).toDateString();
+    }
